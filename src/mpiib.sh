680285b2731495ccec18ce3d8c5296df9e1f5069
kashin_danil <ya.nerkyd112@gmail.com>
2023-02-09 00:55:56 +0300

    Minor comment changes

--- src/mpiib.sh
+++ src/mpiib.sh
@@ -13,13 +13,13 @@ mpiib() {
   fi
   if [[ ! "$options" == *"-i"* ]]; then
     if [[ "$options" == *"tiny"* ]]; then
-      #2000000 iterations, which is around 2 minutes
+      #500000 iterations, which is around 2 minutes
       options="${options/tiny/} -i 500000"
     elif [[ "$options" == *"small"* ]]; then
-      #2000000 iterations, which is around 15 minutes
+      #1000000 iterations, which is around 15 minutes
       options="${options/small/} -i 1000000"
     elif [[ "$options" == *"large"* ]]; then
-      #2000000 iterations, which is around 15 minutes
+      #3000000 iterations, which is around 15 minutes
       options="${options/large/} -i 3000000"
     else #medium
       #2000000 iterations, which is around 10 minutes
