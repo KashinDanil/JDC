1989fd8aee4b96a78e7771b7cf69778a3a542e73
kashin_danil <ya.nerkyd112@gmail.com>
2023-03-15 14:22:02 +0300

    Added overall config bash script and fixed compilation params for cache test

---
+++ Makefile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chmod -R 777 src/HPAS;
+cd src/HPAS;
+./autogen.sh;
+./configure --prefix=$PWD;
+make;
+make install;
+cd ../..;
+
+chmod -R 777 src/osu-micro-benchmarks;
+cd src/osu-micro-benchmarks;
+./configure CC=oshcc CXX=oshcxx;
+make;
+cd ../..;
+
+chmod -R 777 src/Tests/Cache;
+cd src/Tests/Cache;
+make;
+cd ../../..;
