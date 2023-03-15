1a385c935767b96da61a3f9d0318ef396b89dbba
kashin_danil <ya.nerkyd112@gmail.com>
2023-03-15 14:34:03 +0300

    Changed output of the l1cache test

--- src/l1cache.sh
+++ src/l1cache.sh
@@ -13,21 +13,18 @@ l1cache() {
       testOptions="${BASH_REMATCH[3]}"
   else
       #use 1000000000 iterations in l1 cache miss test
-      options=$options" -i 1000000000"
       testOptions="1000000000"
   fi
   if [[ $options =~ ((d|duration) ([0-9]+)) ]]; then
       testOptions=$testOptions" $((BASH_REMATCH[3]/60))"
   else
       #run for 10 minutes with default period and certain memory
-      options=$options" -d 600"
       testOptions=$testOptions" 10"
   fi
   if [[ $options =~ ((h|help)) ]]; then
       testOptions=""
   fi
-  command="$SCRIPT_DIR/src/Tests/Cache/L1Cache $options"
-  echo $command
   command="$SCRIPT_DIR/src/Tests/Cache/L1Cache $testOptions"
+  echo $command
   $command
 }
